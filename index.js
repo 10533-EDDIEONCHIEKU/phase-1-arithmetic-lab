41d20fcabec6415e0b86707e60f26b33396f7110
10533-EDDIEONCHIEKU <onchiekualways@gmail.com>
2023-03-28 14:51:42 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let num1 = 31;
+let num2 = 2;
 
-// Write your code here
+let multiply = num1 * num2; 
+
+console.log(multiply); 
+
+//!random 
+let random = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER) + 1;
+
+console.log(random); 
+//!mod
+let num3 = 14;
+let num4 = 5;
+
+let mod = num3 % num4; 
+
+console.log(mod);
+//! max 
+let set = [4, 8, 16, 20, 2, 6];
+
+let max = Math.max(...set); 
+
+console.log(max); 
